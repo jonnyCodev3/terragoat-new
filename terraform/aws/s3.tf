452a1af581a8711d8cc57cfb6e62dfcfead23851
jonnyCodev <42693227+jonnyCodev@users.noreply.github.com>
2025-07-06 14:52:58 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "data" {
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
+
+
+
+
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
   force_destroy = true
